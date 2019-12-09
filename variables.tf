ef501953d6fe78c42e9c24debd314030fbce8fa5
Simon Elliott <simonelliott@simons-mbp.home>
2019-12-09 17:13:50 +0000

    Initial Commit

--- variables.tf
+++ variables.tf
@@ -2,9 +2,6 @@ variable "bucket_name" {}
 variable "bucket_location" {}
 variable "versioning_enabled" {}
 variable "env" {}
-variable "app_name" {}
 variable "project" {}
-variable "team" {}
-variable "cost_type" {}
 variable "storage_class" {default = "REGIONAL"}
 
